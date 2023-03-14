4c5671fd206e842ac0e4678e8ab640103fa885af
George Karumbi <gkarumbi@gmail.com>
2023-03-14 21:12:47 +0300

    Add a construcor that creates a our coin, call _mint to create our token and send all the tokens to msg.sender

--- lesson-5-homework/BadgerCoin.sol
+++ lesson-5-homework/BadgerCoin.sol
@@ -5,5 +5,7 @@ pragma solidity ^0.8.0;
 import "./openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
 contract BadgerCoin is ERC20{
-    
+     constructor(uint256 initialSupply) ERC20("BadgerCoin","BC"){
+        _mint(msg.sender,initialSupply);
+    }
 }
